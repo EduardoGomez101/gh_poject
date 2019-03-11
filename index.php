7760f663fcd5e520b7fd0a8614de2887a4597e35
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-03-11 16:22:33 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -19,16 +19,36 @@ if ($method == "POST"){
 			$displayText = "FFFF00160000000000000000FFFFFF9300000000074242494E434F00FFA3000200000000000000000065FF83000E000000000000000000650000000000003F02D0000000FF83000E000000000000000000650000000000003F02D0000000FF83000E000000000000000000650000000000003F02D0000000";
 		break;
 		case 'lights on':
-			$speech = "Turning the lights on, now.";
-			$displayText = "Turning the lights on, now.";
+			$speech = "Turning the lights on, now";
+			$displayText = "Turning the lights on, now";
 		break;
 		case 'hey Google':
-			$speech = "Hi Eduardo, glad to hear you again.";
-			$displayText = "Hi Eduardo, glad to hear you again.";
+			$speech = "Hi Eduardo, glad to hear you again";
+			$displayText = "Hi Eduardo, glad to hear you again";
 		break;
 		case 'lights off':
-			$speech = "Turning the lights off.";
-			$displayText = "Turning the lights off.";
+			$speech = "Turning the lights off";
+			$displayText = "Turning the lights off";
+		break;
+		case 'open drapes':
+			$speech = "Opening drapes";
+			$displayText = "Opening drapes";
+		break;
+		case 'close drapes':
+			$speech = "Closing drapes";
+			$displayText = "Closing drapes";
+		break;
+		case 'fan speed high':
+			$speech = "Ok, max speed!";
+			$displayText = "Ok, max speed!";
+		break;
+		case 'fan speed medium':
+			$speech = "Medium speed, alright";
+			$displayText = "Medium speed, alright";
+		break;
+		case 'fan speed low':
+			$speech = "For sure, low speed fan";
+			$displayText = "For sure, low speed fan";
 		break;
 		default:
 			$speech = "I'm still a virtual kid, but soon I will grow up!";
