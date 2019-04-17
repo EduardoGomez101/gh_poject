4902373e54ce870f42a041752134586f4ce7abd9
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-04-17 07:47:55 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ if ($method == "POST"){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	echo $requestBody
-	echo $json
-
 	$text = strtolower($json->result->parameters->text);
 
 	switch ($text){
