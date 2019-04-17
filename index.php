d84cb678f9db5f289f30b0f5a1043d4a78de14ec
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-04-17 11:33:10 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -117,21 +117,26 @@ if ($method == "POST"){
 $response = new stdClass();
 $response->fulfillmentText = "This is a text response";
 
-$response->fulfillmentMessages = array(card);
-$response->fulfillmentMessagesNuevos = array(
-    'type' => 'Feature',
-    'properties' => array(
-      'title' => $programme->title,
-      "marker-symbol" => "music"
-    ),
-    'geometry' => array(
-      'type' => 'Point',
-      'coordinates' => array( 
-        $programme->programme_location->lng,
-        $programme->programme_location->lat
-      )
-    )
-  );
+$response->fulfillmentMessages => array(
+	card->title = "Titulo"
+	);
+
+
+
+//$response->fulfillmentMessagesNuevos = array(
+//    'type' => 'Feature',
+//    'properties' => array(
+//      'title' => $programme->title,
+//      "marker-symbol" => "music"
+//    ),
+//    'geometry' => array(
+//      'type' => 'Point',
+//      'coordinates' => array( 
+//        $programme->programme_location->lng,
+//        $programme->programme_location->lat
+//      )
+//    )
+//  );
 
 
 //$response->fulfillmentMessages->array(card->title = "card title";
