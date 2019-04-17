f203166d109c83f4b186238d3866c6b1c2b8a496
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-04-17 10:18:13 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -61,12 +61,24 @@ if ($method == "POST"){
 		break;
 	}
 
-
-
 $response = new stdClass();
-//$response->fulfillment_text = "fulfillmentText";
-$response->fulfillment_messages = "Texto a presentar";
-$response->source = "webhook";
+$response->fulfillmentText = "This is a text response";
+$response->fulfillmentMessages->card->title = "card title";
+$response->fulfillmentMessages->card->subtitle = "card text";
+$response->fulfillmentMessages->card->imageUri = "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png";
+$response->fulfillmentMessages->card->buttons->text = "button text";
+$response->fulfillmentMessages->card->buttons->text = "https://assistant.google.com/";
+$response->source = "example.com";
+$response->payload->google->expectUserResponse = true;
+$response->payload->google->richResponse->items->simpleResponse->textToSpeech = "This is a simple response";
+$response->payload->facebook->text = "Hello, Facebook";
+$response->payload->slack->text = "This is a text response for Slack";
+$response->outputContexts->name = "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name";
+$response->outputContexts->lifespanCount = 5;
+$response->outputContexts->parameters->param = "param value";
+$response->followupEventInput->name = "event name";
+$response->followupEventInput->languageCode = "es";
+$response->followupEventInput->parameters->param = "param value";
 echo json_encode($response);
 
 //************************************SEGUNDA OPCION
