d8663661dc585804e9c18ffa8913c9d94003d315
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-04-29 10:54:13 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ if ($method == "POST"){
 
 	//$acciones = strtolower($json->queryResult->parameters->acciones);
 	//$termostatos = strtolower($json->queryResult->parameters->termostatos);
-  $number = strtolower($json->queryResult->parameters->number);
+  $contrasenia = strtolower($json->queryResult->parameters->contrasenia);
   $PersonalAutorizado = strtolower($json->queryResult->parameters->PersonalAutorizado);
 
-  if ($number == 12345 && strtolower($PersonalAutorizado) == "eduardo gomez"){
+  if ("contrasenia" == "" && "PersonalAutorizado" == ""){
     $prueba = '{
-      "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
+      "fulfillmentText": "Bienvenid@ a la Inteligencia Artificial de Hospitality Solutions para México y LATAM.",
       "payload": {
         "google": {
           "expectUserResponse": true,
@@ -22,23 +22,23 @@ if ($method == "POST"){
             "items": [
               {
                 "simpleResponse": {
-                  "textToSpeech": "Correcto - Como acceder al Service Mode de un Termostato E528"
+                  "textToSpeech": "Bienvenid@ a la Inteligencia Artificial de Hospitality Solutions para México y LATAM."
                 }
               },
               {
                 "basicCard": {
-                  "title": "E528",
-                  "subtitle": "Termostato",
-                  "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
+                  "title": "IA - Hospitality Solutions",
+                  "subtitle": "México y LATAM",
+                  "formattedText": "",
                   "image": {
-                    "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
-                    "accessibilityText": "Termostato E528"
+                    "url": "https://www.innspire.com/wp-content/uploads/2014/10/Inncom-1.png",
+                    "accessibilityText": "IA - Hospitality Solutions"
                   },
                   "buttons": [
                     {
-                      "title": "Descargar",
+                      "title": "",
                       "openUrlAction": {
-                        "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+                        "url": ""
                       }
                     }
                   ],
@@ -51,44 +51,87 @@ if ($method == "POST"){
       }
     }';
   } else{
-    $prueba = '{
-      "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
-      "payload": {
-        "google": {
-          "expectUserResponse": true,
-          "richResponse": {
-            "items": [
-              {
-                "simpleResponse": {
-                  "textToSpeech": "Incorrecto - Como acceder al Service Mode de un Termostato E528"
-                }
-              },
-              {
-                "basicCard": {
-                  "title": "E528",
-                  "subtitle": "Termostato",
-                  "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
-                  "image": {
-                    "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
-                    "accessibilityText": "Termostato E528"
-                  },
-                  "buttons": [
-                    {
-                      "title": "Descargar",
-                      "openUrlAction": {
-                        "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+    if ($contrasenia == 12345 && strtolower($PersonalAutorizado) == "eduardo gomez"){
+      $prueba = '{
+        "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": "Correcto - Como acceder al Service Mode de un Termostato E528"
+                  }
+                },
+                {
+                  "basicCard": {
+                    "title": "E528",
+                    "subtitle": "Termostato",
+                    "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
+                    "image": {
+                      "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
+                      "accessibilityText": "Termostato E528"
+                    },
+                    "buttons": [
+                      {
+                        "title": "Descargar",
+                        "openUrlAction": {
+                          "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+                        }
                       }
-                    }
-                  ],
-                  "imageDisplayOptions": "CROPPED"
+                    ],
+                    "imageDisplayOptions": "CROPPED"
+                  }
                 }
-              }
-            ]
+              ]
+            }
           }
         }
-      }
-    }';
+      }';
+    } else{
+      $prueba = '{
+        "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": "Incorrecto - Como acceder al Service Mode de un Termostato E528"
+                  }
+                },
+                {
+                  "basicCard": {
+                    "title": "E528",
+                    "subtitle": "Termostato",
+                    "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
+                    "image": {
+                      "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
+                      "accessibilityText": "Termostato E528"
+                    },
+                    "buttons": [
+                      {
+                        "title": "Descargar",
+                        "openUrlAction": {
+                          "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+                        }
+                      }
+                    ],
+                    "imageDisplayOptions": "CROPPED"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }';
+    }
   }
+
+
+    
 
 	$deco = json_decode($prueba);
 	echo json_encode($deco);
