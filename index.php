151c7d1f7fce44dcec557c7cc4dc2a653a574429
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-04-29 09:24:58 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,9 +9,89 @@ if ($method == "POST"){
 
 	$acciones = strtolower($json->queryResult->parameters->acciones);
 	$termostatos = strtolower($json->queryResult->parameters->termostatos);
-  $termostatos = strtolower($json->queryResult->parameters->personalAutorizado);
+  $number = strtolower($json->queryResult->parameters->number);
+  $PersonalAutorizado = strtolower($json->queryResult->parameters->PersonalAutorizado);
 
-  $prueba = '{
+//  if ($number == 12345 && strtolower($PersonalAutorizado) == "eduardo gomez"){
+//    $prueba = '{
+//      "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
+//      "payload": {
+//        "google": {
+//          "expectUserResponse": true,
+//          "richResponse": {
+//            "items": [
+//              {
+//                "simpleResponse": {
+//                  "textToSpeech": "Como acceder al Service Mode de un Termostato E528"
+//                }
+//              },
+//              {
+//                "basicCard": {
+//                  "title": "E528",
+//                  "subtitle": "Termostato",
+//                  "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
+//                  "image": {
+//                    "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
+//                    "accessibilityText": "Termostato E528"
+//                  },
+//                  "buttons": [
+//                    {
+//                      "title": "Descargar",
+//                      "openUrlAction": {
+//                        "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+//                      }
+//                    }
+//                  ],
+//                  "imageDisplayOptions": "CROPPED"
+//                }
+//              }
+//            ]
+//          }
+//        }
+//      }
+//    }';
+//  } else{
+//    $prueba = '{
+//      "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
+//      "payload": {
+//        "google": {
+//          "expectUserResponse": true,
+//          "richResponse": {
+//            "items": [
+//              {
+//                "simpleResponse": {
+//                  "textToSpeech": "Como acceder al Service Mode de un Termostato E528"
+//                }
+//              },
+//              {
+//                "basicCard": {
+//                  "title": "E528",
+//                  "subtitle": "Termostato",
+//                  "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
+//                  "image": {
+//                    "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
+//                    "accessibilityText": "Termostato E528"
+//                  },
+//                  "buttons": [
+//                    {
+//                      "title": "Descargar",
+//                      "openUrlAction": {
+//                        "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
+//                      }
+//                    }
+//                  ],
+//                  "imageDisplayOptions": "CROPPED"
+//                }
+//              }
+//            ]
+//          }
+//        }
+//      }
+//    }';
+//  }
+
+  
+$prueba = '{
     "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
     "payload": {
       "google": {
@@ -48,7 +128,6 @@ if ($method == "POST"){
       }
     }
   }';
-
 
 
 	$deco = json_decode($prueba);
