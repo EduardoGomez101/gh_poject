64e0ed382fb2a4c760f149f2c53caea3e59c6428
EduardoGomez101 <48446942+EduardoGomez101@users.noreply.github.com>
2019-05-06 12:02:25 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,13 @@ if ($method == "POST"){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
+  //LISTADO DE FUNCIONES QUE SE PUEDEN PRESENTAR COMO EJEMPLO
+  //1. Temperatura (Subir a temp definida, Bajar a temp definida, Cambiar temperatura a condiciones especificas, como: frio=18.5°C, fresco=20°C, Templado=22°C, Caliente=25°C)
+  //2. Iluminacion (Encender por zona, encender luminaria en específico, encender todo, apagar todo, dimear una luz a un porcentaje, dimear una luz con un nivel humano)
+  //3. Escenas (Definir una escena)
+  //4. Persianas (Abrir todo, cerrar todo, abrir un poco, cerrar un poco, modo ahorro de energia "cerrando las persianas mientras sea un horario especifico")
+  //5. Opciones adicionales, como encendido de cafetera por medio de una salida de X47
+
   //Listado de variables a emplear
   $Accion = "";
   $Variable = "";
@@ -23,6 +30,18 @@ if ($method == "POST"){
   $number = $json->queryResult->parameters->number;
 
   switch ($Accion){
+    case "habitacion fria":
+      $mensajeRespuesta = "Por supuesto! Colocaré el Set Point a 18.5 grados centígrados.";
+    break;
+    case "habitacion fresca":
+      $mensajeRespuesta = "Muy bien! Definiendo temperatura a 20 grados centígrados.";
+    break;
+    case "habitacion templada":
+      $mensajeRespuesta = "Por supuerto! Cambiando la temperatura a 22 grados centígrados.";
+    break;
+    case "habitacion calida":
+      $mensajeRespuesta = "Vale! Temperatura a 25 grados centígrados.";
+    break;
     case "subir":
       if ($Objeto == "cortinas" || $Objeto == "persianas"){
         $mensajeRespuesta = "Perfecto, estoy abriendo las ".$Objeto;
@@ -31,22 +50,48 @@ if ($method == "POST"){
         $mensajeRespuesta = "Muy bien! Subiendo temperatura a ".$number." grados";
       }
     break;
-    case "apagar":
-      switch($Objeto){
-        case "television": $mensajeRespuesta = "Por supuesto, television apagada."; break;
-        case "luz de baño": $mensajeRespuesta = "Apagando luz de baño. Si detecto presencia, encenderé nuevamente ésta luz."; break;
-        case "todo": $mensajeRespuesta = "Muy bien! Apagando toda la habitación."; break;
+    case "bajar":
+      if ($Objeto == "cortinas" || $Objeto == "persianas"){
+        $mensajeRespuesta = "Cerrando las ".$Objeto;
       }
+      if ($Variable == "temperatura"){
+        $mensajeRespuesta = "Claro! Bajando temperatura a ".$number." grados";
+      }
+    break;
+    case "persianas en modo de ahorro de energia":
+      $mensajeRespuesta = "Muchas gracias! Con tu ayuda, podemos ahorrar energía y con ello ayudar al planeta!. Activando modo de ahorro de energía.";
+    break;
+    case "persianas sin modo de ahorro de energia":
+      $mensajeRespuesta = "Por supuesto! Desactivando modo de ahorro de energía.";
+    break;
+    case "encendido especial":
+      $mensajeRespuesta = "Claro que si! Encendiendo ".$Objeto.".";
     break;
     case "prender":
       switch($Objeto){
         case "television": $mensajeRespuesta = "Claro! Encendiendo la televisión."; break;
         case "luz principal": $mensajeRespuesta = "Muy bien, luz principal encendida."; break;
         case "luz de baño": $mensajeRespuesta = "Encendiendo luz de baño."; break;
+        case "zona de vestidor": $mensajeRespuesta = "Encendiendo luces de la zona de vestidor."; break;
+        case "zona de escritorio": $mensajeRespuesta = "Encendiendo luces del escritorio."; break;
+        case "todo": $mensajeRespuesta = "Claro que si! Encendiendo todas las luces."; break;
+        case "luz de cama": $mensajeRespuesta = "Tal como lo pidas. Encendiendo la luz de cama a un ".$number." porciento."; break;
+        case "luces de forma calida": $mensajeRespuesta = "Muy bien! Ajustando iluminación en modo: Cálido."; break;
+      }
+    break;
+    case "apagar":
+      switch($Objeto){
+        case "television": $mensajeRespuesta = "Por supuesto, television apagada."; break;
+        case "luz de baño": $mensajeRespuesta = "Apagando luz de baño. Si detecto presencia, encenderé nuevamente ésta luz."; break;
+        case "todo": $mensajeRespuesta = "Muy bien! Apagando toda la habitación."; break;
+        case "zona de vestidor": $mensajeRespuesta = "Apagando luces de vestidor."; break;
+        case "zona de escritorio": $mensajeRespuesta = "Apagando luces de la zona de escritorio."; break;
+        case "todo": $mensajeRespuesta = "Perfecto! Apagando todas las luces."; break;
       }
     break;
     case "escena":
       if ($Escena == "dormir") $mensajeRespuesta = "Muy bien, definiendo escena para que puedas dormir tranquilamente. Que descanses. Buenas noches.";
+      if ($Escena == "trabajar") $mensajeRespuesta = "Muy bien, definiendo escena para que puedas trabajar sin interrupciones. Apagando televisión y encendiendo luces de escritorio.";
     break;
   }
 
@@ -87,128 +132,6 @@ if ($method == "POST"){
               }
             }
           }';
-
-
-  //if ($Contrasenia == "" && $PersonalAutorizado == ""){
-  //  $prueba = '{
-  //    "fulfillmentText": "Bienvenid@ a la Inteligencia Artificial de Hospitality Solutions para México y LATAM.",
-  //    "payload": {
-  //      "google": {
-  //        "expectUserResponse": true,
-  //        "richResponse": {
-  //          "items": [
-  //            {
-  //              "simpleResponse": {
-  //                "textToSpeech": "Bienvenid@ a la Inteligencia Artificial de Hospitality Solutions para México y LATAM."
-  //              }
-  //            },
-  //            {
-  //              "basicCard": {
-  //                "title": "Hospitality Solutions - IA",
-  //                "subtitle": "México y LATAM",
-  //                "formattedText": "",
-  //                "image": {
-  //                  "url": "https://www.innspire.com/wp-content/uploads/2014/10/Inncom-1.png",
-  //                  "accessibilityText": "Hospitality Solutions - IA"
-  //                },
-  //                "buttons": [
-  //                  {
-  //                    "title": "",
-  //                    "openUrlAction": {
-  //                      "url": ""
-  //                    }
-  //                  }
-  //                ],
-  //                "imageDisplayOptions": "CROPPED"
-  //              }
-  //            }
-  //          ]
-  //        }
-  //      }
-  //    }
-  //  }';
-  //} else{
-  //  if ($Contrasenia == 12345 && $PersonalAutorizado == "eduardo gomez"){
-  //    $prueba = '{
-  //      "fulfillmentText": "Buen día Sr. '.$json->queryResult->parameters->PersonalAutorizado.', cuál es su duda en específico?",
-  //      "payload": {
-  //        "google": {
-  //          "expectUserResponse": true,
-  //          "richResponse": {
-  //            "items": [
-  //              {
-  //                "simpleResponse": {
-  //                  "textToSpeech": "Buen día Sr. '.$json->queryResult->parameters->PersonalAutorizado.', cuál es su duda en específico?"
-  //                }
-  //              },
-  //              {
-  //                "basicCard": {
-  //                  "title": "",
-  //                  "subtitle": "Hospitality Solutions - IA",
-  //                  "formattedText": "",
-  //                  "image": {
-  //                    "url": "",
-  //                    "accessibilityText": "Termostato E528"
-  //                  },
-  //                  "buttons": [
-  //                    {
-  //                      "title": "",
-  //                      "openUrlAction": {
-  //                        "url": ""
-  //                      }
-  //                    }
-  //                  ],
-  //                  "imageDisplayOptions": "CROPPED"
-  //                }
-  //              }
-  //            ]
-  //          }
-  //        }
-  //      }
-  //    }';
-  //  } else{
-  //    $prueba = '{
-  //      "fulfillmentText": "Para acceder al service mode de un Termostato E528, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón °F/°C\n2. Presione el botón DISPLAY\n3. Presione el botón OFF/AUTO\n4. Libere el botón °F/°C",
-  //      "payload": {
-  //        "google": {
-  //          "expectUserResponse": true,
-  //          "richResponse": {
-  //            "items": [
-  //              {
-  //                "simpleResponse": {
-  //                  "textToSpeech": "Incorrecto - Como acceder al Service Mode de un Termostato E528"
-  //                }
-  //              },
-  //              {
-  //                "basicCard": {
-  //                  "title": "E528",
-  //                  "subtitle": "Termostato",
-  //                  "formattedText": "Para acceder al service mode de un **Termostato E528**, siga los siguientes pasos:\n1. Presione y mantenga presionado el botón __°F/°C__\n2. Presione el botón __DISPLAY__\n3. Presione el botón __OFF/AUTO__\n4. Libere el botón __°F/°C__",
-  //                  "image": {
-  //                    "url": "https://sc01.alicdn.com/kf/HTB1f9srKpXXXXXsXXXXq6xXFXXXo/e4-Smart-Digital-Thermostat-E528.jpg_350x350.jpg",
-  //                    "accessibilityText": "Termostato E528"
-  //                  },
-  //                  "buttons": [
-  //                    {
-  //                      "title": "Descargar",
-  //                      "openUrlAction": {
-  //                        "url": "https://inncom.com/images/TechnicalDocs/E528_-Product-Guide_Rev8.0_20MAR17.pdf"
-  //                      }
-  //                    }
-  //                  ],
-  //                  "imageDisplayOptions": "CROPPED"
-  //                }
-  //              }
-  //            ]
-  //          }
-  //        }
-  //      }
-  //    }';
-  //  }
-  //}
-
-
-    
 
 	$deco = json_decode($prueba);
 	echo json_encode($deco);
